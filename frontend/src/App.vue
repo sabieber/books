6640e35019faaa1fce6f463eaa519e84be22a2cd
Sascha Bieberstein <sbi@scireum.de>
2025-03-02 22:05:53 +0100

    Add bottom navbar for mobile and highlight active item

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,41 +5,62 @@
         <RouterLink to="/">Books</RouterLink>
       </a>
     </div>
-    <div class="flex-none">
+    <div class="flex-none hidden sm:flex">
       <ul class="menu menu-horizontal px-1">
         <li class="tooltip tooltip-bottom" data-tip="Home">
-          <RouterLink to="/">
-            <HomeIcon class="size-6"/>
+          <RouterLink to="/" :class="{ 'menu-active': $route.path === '/' }">
+            <HomeIcon class="size-6" />
           </RouterLink>
         </li>
         <li class="tooltip tooltip-bottom" data-tip="Search">
-          <RouterLink to="/search">
-            <MagnifyingGlassIcon class="size-6"/>
+          <RouterLink to="/search" :class="{ 'menu-active': $route.path === '/search' }">
+            <MagnifyingGlassIcon class="size-6" />
           </RouterLink>
         </li>
         <li class="tooltip tooltip-bottom" data-tip="Library">
-          <RouterLink to="/library">
-            <BookOpenIcon class="size-6"/>
+          <RouterLink to="/library" :class="{ 'menu-active': $route.path === '/library' }">
+            <BookOpenIcon class="size-6" />
           </RouterLink>
         </li>
         <li class="tooltip tooltip-bottom" data-tip="Logout">
           <a @click="logout">
-            <UserIcon class="size-6"/>
+            <UserIcon class="size-6" />
           </a>
         </li>
       </ul>
     </div>
   </div>
 
-  <RouterView/>
+  <RouterView />
+
+  <div class="dock dock-sm sm:hidden">
+    <button :class="{ 'dock-active': $route.path === '/' }">
+      <RouterLink to="/">
+        <HomeIcon class="size-6" />
+      </RouterLink>
+    </button>
+    <button :class="{ 'dock-active': $route.path === '/search' }">
+      <RouterLink to="/search">
+        <MagnifyingGlassIcon class="size-6" />
+      </RouterLink>
+    </button>
+    <button :class="{ 'dock-active': $route.path === '/library' }">
+      <RouterLink to="/library">
+        <BookOpenIcon class="size-6" />
+      </RouterLink>
+    </button>
+    <button @click="logout">
+      <UserIcon class="size-6" />
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
 import router from "@/router";
-import {MagnifyingGlassIcon, HomeIcon, BookOpenIcon, UserIcon} from "@heroicons/vue/16/solid";
+import { MagnifyingGlassIcon, HomeIcon, BookOpenIcon, UserIcon } from "@heroicons/vue/16/solid";
 
 export default {
-  components: {MagnifyingGlassIcon, HomeIcon, BookOpenIcon, UserIcon},
+  components: { MagnifyingGlassIcon, HomeIcon, BookOpenIcon, UserIcon },
   methods: {
     logout() {
       localStorage.removeItem('token')
