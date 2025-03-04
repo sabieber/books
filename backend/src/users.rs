cdf0092643ca21446c488c48a2cddc57e889635e
Sascha Bieberstein <sbi@scireum.de>
2025-03-04 22:09:08 +0100

    Switch from argonaut to argon2 crate for password hashing
    The argonaut crate seems to be highly outdated and depends on some crates with open CVEs.

--- backend/src/users.rs
+++ backend/src/users.rs
@@ -3,14 +3,16 @@ use crate::models::User;
 use crate::schema::users::dsl::users;
 use crate::schema::users::name;
 use crate::ErrorResponse;
-use argonautica::{Hasher, Verifier};
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
 use axum::extract::rejection::JsonRejection;
 use axum::routing::post;
 use axum::{extract::Json, http::StatusCode, response::IntoResponse, Router};
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::env;
 use uuid::Uuid;
 
 pub(crate) fn register_routes(router: Router) -> Router {
@@ -52,14 +54,11 @@ pub(crate) async fn register(
         }
     };
 
-    let secret = env::var("PASSWORD_SECRET").expect("PASSWORD_SECRET must be set");
-
-    let mut hasher = Hasher::default();
-    let password_hash = hasher
-        .with_password(payload.password.clone())
-        .with_secret_key(secret)
-        .hash()
-        .unwrap();
+    let salt = SaltString::generate(&mut OsRng);
+    let password_hash = Argon2::default()
+        .hash_password(payload.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
 
     let new_user = User {
         id: Uuid::new_v4(),
@@ -109,8 +108,6 @@ pub struct LoginResponse {
 /// - `username`: The name of the user to log in.
 /// - `password`: The password of the user to log in.
 pub(crate) async fn login(Json(payload): Json<LoginRequest>) -> impl IntoResponse {
-    let secret = env::var("PASSWORD_SECRET").expect("PASSWORD_SECRET must be set");
-
     let connection = &mut connect();
 
     let user: User = match users.filter(name.eq(&payload.username)).first(connection) {
@@ -125,13 +122,11 @@ pub(crate) async fn login(Json(payload): Json<LoginRequest>) -> impl IntoRespons
         }
     };
 
-    let mut verifier = Verifier::default();
-    let is_valid = verifier
-        .with_hash(user.password)
-        .with_password(payload.password)
-        .with_secret_key(secret)
-        .verify()
-        .unwrap();
+    // TODO dont use unwrap
+    let parsed_hash = PasswordHash::new(&user.password).unwrap();
+    let is_valid = Argon2::default()
+        .verify_password(payload.password.as_bytes(), &parsed_hash)
+        .is_ok();
 
     if is_valid {
         (
